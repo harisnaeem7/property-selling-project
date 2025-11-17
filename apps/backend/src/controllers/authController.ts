dd5780711ffe302fd206c9f06a7acbbf7f5dc47c
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-17 14:46:12 -0700

    Login route created

--- apps/backend/src/controllers/authController.ts
+++ apps/backend/src/controllers/authController.ts
@@ -2,38 +2,63 @@ import { Request, Response } from "express";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
 import User from "../models/Users";
+import { error } from "console";
 
 export const registerUser = async (req: Request, res: Response) => {
-  const { firstName, lastName, email, password, role, phone, createdAt } =
-    req.body || {};
+  try {
+    const { firstName, lastName, email, password, role, phone, createdAt } =
+      req.body || {};
 
-  if (!firstName || !lastName || !email) {
+    if (!firstName || !lastName || !email) {
+      return res
+        .status(401)
+        .json({ message: "Please fill all the required fields." });
+    }
+    const existingUser = await User.findOne({ email });
+
+    if (!existingUser) {
+      const hashedPassword = await bcrypt.hash(password, 10);
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password: hashedPassword,
+        role,
+        phone,
+        createdAt: new Date(),
+      });
+      const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
+        expiresIn: "1h",
+      });
+      return res
+        .status(200)
+        .json({ message: `User with email ${email} created successfully!` });
+    } else {
+      return res
+        .status(401)
+        .json({ message: `User with email ${email} already exists!` });
+    }
+  } catch (err: any) {
     return res
-      .status(404)
-      .json({ message: "Please fill all the required fields." });
+      .status(500)
+      .json({ message: "Server error", error: err.message });
   }
-  const existingUser = await User.findOne({ email });
+};
 
-  if (!existingUser) {
-    const hashedPassword = await bcrypt.hash(password, 10);
-    const user = await User.create({
-      firstName,
-      lastName,
-      email,
-      password: hashedPassword,
-      role,
-      phone,
-      createdAt: new Date(),
-    });
-    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET!, {
-      expiresIn: "1h",
-    });
+export const loginUser = async (req: Request, res: Response) => {
+  try {
+    const { email, password } = req.body || {};
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+    } else {
+      return res
+        .status(401)
+        .json({ message: "Please enter valid email or password." });
+    }
+  } catch (err: any) {
     return res
-      .status(200)
-      .json({ message: `User with email ${email} created successfully!` });
-  } else {
-    return res
-      .status(404)
-      .json({ message: `User with email ${email} already exists!` });
+      .status(500)
+      .json({ message: "Server error", error: err.message });
   }
 };
