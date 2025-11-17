105acd47c91818fa5f233782150fbf83ce284125
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-17 13:04:48 -0700

    code updates

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -2,6 +2,7 @@ import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
 const connectDB = require("./db");
+import userRoutes from "./routes/authRoutes";
 
 dotenv.config();
 const app = express();
@@ -10,10 +11,13 @@ connectDB();
 
 app.use(cors());
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get("/", (req, res) => {
   res.send("Backend is running now  ");
 });
+
+app.use("/auth", userRoutes);
 
 const PORT = process.env.PORT || 5009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
