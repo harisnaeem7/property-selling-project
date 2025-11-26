704899d03039ffc6b18def0ac4c23ae9ee20b454
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-25 19:09:40 -0700

    Updated Routes (Frontend)

--- apps/frontend/src/routes/Routes.tsx
+++ apps/frontend/src/routes/Routes.tsx
@@ -1,7 +1,9 @@
-import { Routes, Route } from "react-router";
+import { Routes, Route } from "react-router-dom";
 import Register from "../pages/account/Register";
 import Login from "../pages/account/Login";
 import Dashboard from "../pages/account/Dashboard";
+import ProtectedRoute from "./ProtectedRoutes";
+import { Outlet } from "react-router-dom";
 
 const Routing = () => {
   return (
@@ -10,7 +12,14 @@ const Routing = () => {
         <Route index element={<Login />} />
         <Route path="register" element={<Register />} />
       </Route>
-      <Route path="user">
+      <Route
+        path="user"
+        element={
+          <ProtectedRoute>
+            <Outlet />
+          </ProtectedRoute>
+        }
+      >
         <Route path="dashboard" element={<Dashboard />} />
       </Route>
     </Routes>
