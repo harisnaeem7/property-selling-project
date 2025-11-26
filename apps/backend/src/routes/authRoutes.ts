fb98194e595e41e412ff3e8d436fc1db5ffd1a5e
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-25 18:56:10 -0700

    Updated Auth Route

--- apps/backend/src/routes/authRoutes.ts
+++ apps/backend/src/routes/authRoutes.ts
@@ -1,5 +1,6 @@
-import express from "express";
+import express, { NextFunction, Request, Response } from "express";
 import { loginUser, registerUser } from "../controllers/authController";
+import { verifyToken } from "../middleware/protectedroutes";
 
 const authRouter = express.Router();
 
