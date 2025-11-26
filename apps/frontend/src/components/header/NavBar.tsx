9f8efea945d87fb194a35ae34fd410e8c4320d25
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-25 19:08:49 -0700

    Updated react router dom import

--- apps/frontend/src/components/header/NavBar.tsx
+++ apps/frontend/src/components/header/NavBar.tsx
@@ -12,7 +12,7 @@ import Button from "@mui/material/Button";
 import Tooltip from "@mui/material/Tooltip";
 import MenuItem from "@mui/material/MenuItem";
 import AdbIcon from "@mui/icons-material/Adb";
-import { NavLink } from "react-router";
+import { NavLink } from "react-router-dom";
 import { AuthContext } from "../../context/AuthContext";
 import { useContext } from "react";
 
