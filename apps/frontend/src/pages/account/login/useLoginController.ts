dacc90e3724d450b87141a2482e05975103f6c0e
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-11-27 15:02:56 -0700

    Added login controller frontend

---
+++ apps/frontend/src/pages/account/login/useLoginController.ts
@@ -0,0 +1,54 @@
+import { useState, useContext } from "react";
+import { useNavigate } from "react-router-dom";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+import { type SubmitHandler } from "react-hook-form";
+
+import { AuthContext } from "../../../context/AuthContext";
+import { loginSchema } from "./login.schema";
+import { LogInUser } from "../../../api/auth";
+import { type UserInput } from "./login.type";
+
+export const useLoginController = () => {
+  const [serverError, setServerError] = useState<string | null>(null);
+  const [successMessage, setSuccessMessage] = useState<string | null>(null);
+  const [backedError, setBackendError] = useState<string | null>(null);
+  const navigate = useNavigate();
+  const auth = useContext(AuthContext);
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: yupResolver(loginSchema) });
+
+  const onSubmit: SubmitHandler<UserInput> = async (data) => {
+    setBackendError(null);
+    setServerError(null);
+    setSuccessMessage(null);
+    try {
+      const response = await LogInUser(data);
+      console.log(response);
+      localStorage.setItem("token", response.token);
+      localStorage.setItem("user", response.email);
+      setSuccessMessage("Logged in successfully!");
+      auth?.login(response.token, response.email);
+      navigate("/user/dashboard", { replace: true });
+    } catch (err: any) {
+      console.log(err.response?.data?.message);
+      if (err.response?.data?.message) {
+        setBackendError(err.response?.data?.message);
+      } else {
+        setServerError("Something went wrong. Please try again");
+      }
+    }
+  };
+  return {
+    handleSubmit,
+    onSubmit,
+    register,
+    errors,
+    backedError,
+    serverError,
+    successMessage,
+  };
+};
