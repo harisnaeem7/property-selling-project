983b09fe7e6bed3c1ab14cc933081a9944ea9b0d
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-05 20:42:39 -0700

    updated api calls

--- apps/frontend/src/api/auth.ts
+++ apps/frontend/src/api/auth.ts
@@ -1,33 +1,35 @@
-import axios from "axios";
 import api from "./api";
 
-const API_URL = import.meta.env.VITE_API_URL;
-
 export const RegisterUser = async (data: any) => {
-  const response = await axios.post(`${API_URL}/auth/register`, data);
-  return response.data;
+  const response = await api.post(`/auth/register`, data);
+  return response;
 };
 export const LogInUser = async (data: any) => {
-  const response = await axios.post(`${API_URL}/auth/login`, data);
-  return response.data;
+  const response = await api.post(`/auth/login`, data);
+  return response;
 };
 
 export const ForgotPassword = async (data: any) => {
-  const response = await axios.post(`${API_URL}/auth/forgot`, data);
-  return response.data;
+  const response = await api.post(`/auth/forgot`, data);
+  return response;
 };
 
 export const ResetPassword = async (data: any, token: string) => {
-  const response = await axios.post(`${API_URL}/auth/reset/${token}`, data);
-  return response.data;
+  const response = await api.post(`/auth/reset/${token}`, data);
+  return response;
 };
 
 export const setupMFA = async () => {
-  const response = await api.post(`${API_URL}/auth/mfa/setup`);
-  return response.data;
+  const response = await api.post(`/auth/mfa/setup`);
+  return response;
 };
 
 export const verifyMFA = async (data: any) => {
-  const response = await api.post(`${API_URL}/auth/mfa/verify`, data);
-  return response.data;
+  const response = await api.post(`/auth/mfa/verify`, data);
+  return response;
+};
+
+export const dashboardApi = async () => {
+  const response = await api.get(`/dashboard`);
+  return response;
 };
