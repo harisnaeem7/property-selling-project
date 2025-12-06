0224af52222572cc81c36dc8841f866fc60f041a
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-05 20:40:50 -0700

    Added middle for authorization

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,6 +5,7 @@ const connectDB = require("./db");
 import authRouter from "./routes/authRoutes";
 import { verifyToken } from "./middleware/protectedroutes";
 import userRoutes from "./routes/userRoutes";
+import dashboardRoute from "./routes/dashboard";
 
 dotenv.config();
 const app = express();
@@ -22,6 +23,7 @@ app.get("/", (req, res) => {
 
 app.use("/auth", authRouter);
 app.use("/user", userRoutes);
+app.use("/", dashboardRoute);
 
 const PORT = process.env.PORT || 5009;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- apps/backend/src/middleware/protectedroutes.ts
+++ apps/backend/src/middleware/protectedroutes.ts
@@ -1,19 +1,38 @@
 import jwt from "jsonwebtoken";
-import { Response, NextFunction } from "express";
+import { Response, NextFunction, Request } from "express";
+import User from "../models/Users";
+import { AuthenticatedRequest } from "./authorization";
 
-export const verifyToken = (req: any, res: Response, next: NextFunction) => {
-  const header = req.headers.authorization;
+export const verifyToken = async (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
+  const authorizationHeader = req.headers["authorization"];
 
-  if (!header || !header.startsWith("Bearer "))
+  if (
+    !authorizationHeader ||
+    !(authorizationHeader as string)?.startsWith("Bearer ")
+  )
     return res.status(401).json({ message: "Not Authorized " });
 
-  const token = header.split(" ")[1];
+  const token = (authorizationHeader as string).split(" ")[1];
+  if (!token) {
+    return res.status(401).json({ message: "Token not found" });
+  }
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET!);
-    req.user = decoded;
-    console.log(decoded);
+
+    const existingUser = await User.findById((decoded as any).id);
+
+    if (!existingUser) {
+      return res.status(401).json({ message: "User does not exists!" });
+    }
+
+    (req as AuthenticatedRequest).user = existingUser;
+
     next();
-  } catch {
+  } catch (err: any) {
     return res.status(401).json({ message: "Invalid token" });
   }
 };
