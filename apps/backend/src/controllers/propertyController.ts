d490939d29df3897f618bc0d2b7de7e2362accee
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-08 17:05:08 -0700

    added property route controller

---
+++ apps/backend/src/controllers/propertyController.ts
@@ -0,0 +1,63 @@
+import { Request, Response } from "express";
+import { Property } from "../models/Property";
+
+export const createProperty = async (req: Request, res: Response) => {
+  const {
+    // ownerId,
+    title,
+    price,
+    purpose,
+    propertyType,
+    bedrooms,
+    bathrooms,
+    utilities,
+    address,
+    city,
+    description,
+    images,
+    status,
+    createdAt,
+    updatedAt,
+  } = req.body || {};
+
+  if (
+    !title ||
+    !price ||
+    !purpose ||
+    !propertyType ||
+    !bedrooms ||
+    !bathrooms ||
+    !utilities ||
+    !address ||
+    !city ||
+    !description ||
+    !images ||
+    !status
+  ) {
+    return res.status(400).json({ message: "Missing required fields" });
+  }
+
+  try {
+    const property = await Property.create({
+      //ownerId,
+      title,
+      price,
+      purpose,
+      propertyType,
+      bedrooms,
+      bathrooms,
+      utilities,
+      address,
+      city,
+      description,
+      images,
+      status,
+      createdAt,
+      updatedAt,
+    });
+  } catch (err) {
+    return res.status(403).json({ message: err });
+  }
+
+  return res.status(200).json({ message: "property created successfully!" });
+};
