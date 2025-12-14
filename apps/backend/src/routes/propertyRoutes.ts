d5c0879085cdb473f880419062f1df1f047989d0
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-13 20:54:42 -0700

    updated property route

--- apps/backend/src/routes/propertyRoutes.ts
+++ apps/backend/src/routes/propertyRoutes.ts
@@ -3,12 +3,16 @@ import { createProperty } from "../controllers/propertyController";
 import { validateRequest } from "../middleware/validate";
 import { createPropertyDTO } from "../dtos/property/createProperty";
 import { verifyToken } from "../middleware/protectedroutes";
+import { upload } from "../middleware/upload";
+import { testUpload } from "../controllers/propertyController";
 
 export const propertyRoutes = express.Router();
 
 propertyRoutes.post(
   "/create-property",
   verifyToken,
-  validateRequest(createPropertyDTO),
+  // validateRequest(createPropertyDTO),
+  upload.array("images", 10),
+  //testUpload
   createProperty
 );
