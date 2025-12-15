ec405c9f26da85cf58376b9569a0f491e4ea9d93
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-14 22:03:30 -0700

    updated property route and added reflect metadata

--- apps/backend/src/routes/propertyRoutes.ts
+++ apps/backend/src/routes/propertyRoutes.ts
@@ -4,15 +4,13 @@ import { validateRequest } from "../middleware/validate";
 import { createPropertyDTO } from "../dtos/property/createProperty";
 import { verifyToken } from "../middleware/protectedroutes";
 import { upload } from "../middleware/upload";
-import { testUpload } from "../controllers/propertyController";
 
 export const propertyRoutes = express.Router();
 
 propertyRoutes.post(
   "/create-property",
   verifyToken,
-  // validateRequest(createPropertyDTO),
   upload.array("images", 10),
-  //testUpload
+  validateRequest(createPropertyDTO),
   createProperty
 );
