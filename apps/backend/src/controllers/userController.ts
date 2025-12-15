0c729c9a119867f37d9860be5c9514b5fde54b91
Muhammad Haris Naeem <haris_eng@outlook.com>
2025-12-14 22:02:53 -0700

    Minor code changes

--- apps/backend/src/controllers/userController.ts
+++ apps/backend/src/controllers/userController.ts
@@ -9,11 +9,10 @@ interface AuthRequest extends Request {
 
 export const UserController = async (req: AuthRequest, res: Response) => {
   const userId = req.user?.id;
-  console.log(userId);
+
   const user = await User.findById(userId).select(
     "firstName lastName email role, _id, isMfaEnabled"
   );
-  console.log(user);
 
   return res.status(200).json({ user });
 };
